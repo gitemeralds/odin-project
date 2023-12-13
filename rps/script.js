36690719d4c9177e2303509402564374dea3ccc8
Josue Alamo <gitemerald@proton.me>
2023-12-12 21:59:39 -0600

    Added function that gets player's choice for the game and tested it

--- rps/script.js
+++ rps/script.js
@@ -4,3 +4,14 @@ const getComputerChoice = () => {
   ];
   return computerChoice;
 };
+
+const getPlayerChoice = () => {
+  let validMoves = ["rock", "paper", "scissors"];
+  let playerChoice = prompt("Please enter yout move: ");
+  if (playerChoice != null && validMoves.includes(playerChoice.toLowerCase())) {
+    return playerChoice.toLowerCase();
+  } else {
+    alert("That move is invalid.");
+    return getPlayerChoice();
+  }
+};
