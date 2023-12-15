ce98d367f18ffbf62a44bd525a843f780a7bdfe3
Josue Alamo <gitemerald@proton.me>
2023-12-14 20:40:23 -0600

    Added game function to make the game playable.

--- rps/script.js
+++ rps/script.js
@@ -17,11 +17,10 @@ const getPlayerChoice = () => {
   }
 };
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
-
 const playRound = (playerChoice, computerChoice) => {
   let result;
+  alert("Player's choice is " + playerChoice);
+  alert("Computer's choice is " + computerChoice);
   if (playerChoice == "rock") {
     if (computerChoice == "rock") {
       result = "It's a tie!";
@@ -47,13 +46,82 @@ const playRound = (playerChoice, computerChoice) => {
       result = "You win this round!";
     }
   }
-  alert(result);
-  return result;
+  if (result == "It's a tie!") {
+    alert("This round is a tie!");
+    playerChoice = getPlayerChoice();
+    computerChoice = getComputerChoice();
+    return playRound(playerChoice, computerChoice);
+  } else {
+    alert(result);
+    return result;
+  }
 };
 
-playRound(playerChoice, computerChoice);
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let playerChoice = getPlayerChoice();
+  let computerChoice = getComputerChoice();
+  let roundResult = playRound(playerChoice, computerChoice);
+  if (roundResult === "You win this round!") {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  alert("Computer score is: " + computerScore);
+  alert("Player score is: " + playerScore);
+  playerChoice = getPlayerChoice();
+  computerChoice = getComputerChoice();
+  roundResult = playRound(playerChoice, computerChoice);
+  if (roundResult === "You win this round!") {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  alert("Computer score is: " + computerScore);
+  alert("Player score is: " + playerScore);
+
+  playerChoice = getPlayerChoice();
+  computerChoice = getComputerChoice();
+  roundResult = playRound(playerChoice, computerChoice);
+  if (roundResult === "You win this round!") {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  alert("Computer score is: " + computerScore);
+  alert("Player score is: " + playerScore);
+
+  playerChoice = getPlayerChoice();
+  computerChoice = getComputerChoice();
+  roundResult = playRound(playerChoice, computerChoice);
+  if (roundResult === "You win this round!") {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  alert("Computer score is: " + computerScore);
+  alert("Player score is: " + playerScore);
+
+  playerChoice = getPlayerChoice();
+  computerChoice = getComputerChoice();
+  roundResult = playRound(playerChoice, computerChoice);
+  if (roundResult === "You win this round!") {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  alert("Computer score is: " + computerScore);
+  alert("Player score is: " + playerScore);
+  if (playerScore > computerScore) {
+    alert("You win the game!");
+  } else {
+    alert("You lose the game!");
+  }
+};
 
 /*
 TODO:
--test play round function
+-add alert "Player score is:" and also for the computer score.
+-add alert that shows what the computer chose and what the player chose.
 */
