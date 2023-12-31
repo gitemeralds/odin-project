4fdcb02b6902422ae354a9c6ed24c062e78c3605
Josue Alamo <gitemerald@proton.me>
2023-12-31 15:38:43 -0600

    Added score display's text and added scores counters

--- rps/script.js
+++ rps/script.js
@@ -1,18 +1,28 @@
+//DOM elements and variables
 let scoreDisplaysContainer = document.createElement("div");
 document.body.appendChild(scoreDisplaysContainer);
 let computerScoreDisplay = document.createElement("div");
-document.scoreDisplaysContainer.appendChild(computerScoreDisplay);
+scoreDisplaysContainer.appendChild(computerScoreDisplay);
 let playerScoreDisplay = document.createElement("div");
-document.scoreDisplaysContainer.appendChild(playerScoreDisplay);
+scoreDisplaysContainer.appendChild(playerScoreDisplay);
 let rpsButtonsContainer = document.createElement("div");
 document.body.appendChild(rpsButtonsContainer);
 let rockButton = document.createElement("button");
-document.rpsButtonsContainer.appendChild(rockButton);
+rpsButtonsContainer.appendChild(rockButton);
 let scissorsButton = document.createElement("button");
-document.rpsButtonsContainer.appendChild(scissorsButton);
+rpsButtonsContainer.appendChild(scissorsButton);
 let paperButton = document.createElement("button");
-document.rpsButtonsContainer.appendChild(paperButton);
+rpsButtonsContainer.appendChild(paperButton);
 let resetButton = document.createElement("button");
+let computerScore = 0;
+let playerScore = 0;
+//DOM elements classes
+scoreDisplaysContainer.classList.add("scoreDisplaysContainer");
+computerScoreDisplay.classList.add("computerScoreDisplay");
+playerScoreDisplay.classList.add("playerScoreDisplay");
+//Text content
+computerScoreDisplay.textContent = "Computer Score: " + computerScore;
+playerScoreDisplay.textContent = "Player Score: " + playerScore;
 
 const getComputerChoice = () => {
   let computerChoice = ["rock", "paper", "scissors"][
