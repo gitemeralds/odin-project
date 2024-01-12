916ed6cd7ab92fcfe1a35994efa26fd3e1f8dc1e
Josue Alamo <gitemerald@proton.me>
2024-01-11 22:42:54 -0600

    Added text content to all buttons and selected them on javascript

--- calculator/script.js
+++ calculator/script.js
@@ -37,3 +37,36 @@ rows = document.getElementsByClassName("row");
 for (let i = 0; i < rows.length; i++) {
   grid.appendChild(rows[i]);
 }
+
+const plusButton = document.querySelector(".row4 .button1");
+plusButton.textContent = " + ";
+const minusButton = document.querySelector(".row3 .button1");
+minusButton.textContent = " - ";
+const timesButton = document.querySelector(".row2 .button1");
+timesButton.textContent = " x ";
+const divisionButton = document.querySelector(".row1 .button1");
+divisionButton.textContent = " \u00F7 ";
+const button1 = document.querySelector(".row4 .button4");
+button1.textContent = "1";
+const button2 = document.querySelector(".row4 .button3");
+button2.textContent = "2";
+const button3 = document.querySelector(".row4 .button2");
+button3.textContent = "3";
+const button4 = document.querySelector(".row3 .button4");
+button4.textContent = "4";
+const button5 = document.querySelector(".row3 .button3");
+button5.textContent = "5";
+const button6 = document.querySelector(".row3 .button2");
+button6.textContent = "6";
+const button7 = document.querySelector(".row2 .button4");
+button7.textContent = "7";
+const button8 = document.querySelector(".row2 .button3");
+button8.textContent = "8";
+const button9 = document.querySelector(".row2 .button2");
+button9.textContent = "9";
+const negativeButton = document.querySelector(".row1 .button4");
+negativeButton.textContent = "-/+";
+const button0 = document.querySelector(".row1 .button3");
+button0.textContent = "0";
+const dotButton = document.querySelector(".row1 .button2");
+dotButton.textContent = ".";
